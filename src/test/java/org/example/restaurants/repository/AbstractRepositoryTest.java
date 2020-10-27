7c15476ec6143485768f59d88e56bc48f3a4ea9f
m_lependin <m_lependin@mail.ru>
2020-10-27 22:05:13 +0300

    Add TimingExtension to check the time of test execution
    add AbstractRepositoryTest

---
+++ src/test/java/org/example/restaurants/repository/AbstractRepositoryTest.java
@@ -0,0 +1,16 @@
+package org.example.restaurants.repository;
+
+import org.example.restaurants.TimingExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig(locations = {
+        "classpath:spring/spring-db.xml"
+})
+@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
+@ExtendWith(value = TimingExtension.class)
+public abstract class AbstractRepositoryTest {
+
+}
