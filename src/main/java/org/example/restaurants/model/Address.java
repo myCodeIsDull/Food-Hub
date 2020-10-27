2108e0f4387be1d21ce1aae2bd14c10ffeaed158
m_lependin <m_lependin@mail.ru>
2020-10-27 12:27:47 +0300

    Changes in Restaurant, make Address embeddable

--- src/main/java/org/example/restaurants/model/Address.java
+++ src/main/java/org/example/restaurants/model/Address.java
@@ -1,71 +1,37 @@
 package org.example.restaurants.model;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
-import java.util.Objects;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Entity
-@Table(uniqueConstraints = {@UniqueConstraint(columnNames = {"phone"}, name = "unique_phone_number")})
-public class Address extends AbstractBaseEntity {
+@Embeddable
+@NoArgsConstructor
+@Getter
+@Setter
+public class Address {
 
-    @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "rest_id", nullable = false)
-    private Restaurant restaurant;
+  @Column(nullable = false)
+  @NotBlank
+  @Size(min = 1, max = 20)
+  private String city;
 
-    @Column(name = "city", nullable = false)
-    @NotBlank
-    @Size(min = 1, max = 20)
-    private String city;
+  @Column(nullable = false)
+  @NotBlank
+  @Size(min = 5, max = 50)
+  private String street;
 
-    @Column(name = "street", nullable = false)
-    @NotBlank
-    @Size(min = 5, max = 50)
-    private String street;
+  @Column(nullable = false)
+  @NotBlank
+  @Size(min = 1, max = 20)
+  private String house;
 
-    @Column(name = "house", nullable = false)
-    @NotBlank
-    @Size(min = 1, max = 20)
-    private String house;
+  @Column(nullable = false)
+  @NotBlank
+  @Size(min = 5, max = 20)
+  private String phone;
 
-    @Column(name = "phone", nullable = false)
-    @NotBlank
-    @Size(min = 5, max = 20)
-    private String phone;
-
-    public Address(String city, String street, String house, String phone) {
-        this.city = city;
-        this.street = street;
-        this.house = house;
-        this.phone = phone;
-    }
-
-    public Address(Integer id, Restaurant restaurant, String city, String street, String house, String phone) {
-        super(id);
-        this.restaurant = restaurant;
-        this.city = city;
-        this.street = street;
-        this.house = house;
-        this.phone = phone;
-    }
-
-    protected Address() {
-    }
-
-
-    public void setRestaurant(Restaurant restaurant) {
-
-        this.restaurant = restaurant;
-    }
-
-    @Override
-    public String toString() {
-        return "Address{" +
-                "city='" + city + '\'' +
-                ", street='" + street + '\'' +
-                ", house=" + house +
-                ", phone='" + phone + '\'' +
-                ", id=" + id +
-                '}';
-    }
 }
--- src/main/java/org/example/restaurants/model/Restaurant.java
+++ src/main/java/org/example/restaurants/model/Restaurant.java
@@ -1,88 +1,42 @@
 package org.example.restaurants.model;
 
-import javax.persistence.*;
-import java.util.Collection;
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.util.Date;
-import java.util.LinkedList;
-import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@Table(name = "restaurants")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 @NamedQueries({
-        @NamedQuery(name = Restaurant.GET_BY_PHONE, query = "SELECT r FROM Restaurant r LEFT JOIN FETCH r.address WHERE r.address.phone=?1"),
-        @NamedQuery(name = Restaurant.GET_ALL, query = "SELECT r FROM Restaurant r LEFT JOIN FETCH r.address a order by r.name"),
-        @NamedQuery(name = Restaurant.GET_ALL_WITH_MENUS, query = "SELECT DISTINCT r FROM Restaurant r LEFT JOIN FETCH r.address LEFT JOIN FETCH r.menuList")
-
+    @NamedQuery(name = Restaurant.GET_ALL, query = "SELECT r FROM Restaurant r"),
 })
+@Table(name = "restaurants", uniqueConstraints = {
+    @UniqueConstraint(columnNames = {"phone"}, name = "unique_phone_number")})
 public class Restaurant extends AbstractNamedEntity {
-    public static final String GET_BY_PHONE = "Restaurant.getByPhone";
-    public static final String GET_ALL = "Restaurant.getAll";
-    public static final String GET_ALL_WITH_MENUS = "Restaurant.getWithMenus";
 
-    @Column(name = "registered", nullable = false, columnDefinition = "timestamp default now()")
-    private Date registered = new Date();
+  public static final String GET_ALL = "Restaurant.getAll";
 
-    @OneToOne(mappedBy = "restaurant", fetch = FetchType.LAZY, cascade = CascadeType.ALL, optional = false)
-    private Address address;
+  @Column(name = "registered", nullable = false, columnDefinition = "timestamp default now()")
+  @JsonFormat(pattern = "yyyy-MM-dd")
+  private Date registered = new Date();
 
-    @Column(name = "enabled", nullable = false, columnDefinition = "boolean default true")
-    private boolean enabled = true;
+  @Embedded
+  private Address address;
 
-    @OneToMany(mappedBy = "restaurant", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Menu> menuList;
-
-    public Restaurant() {
-    }
-
-    public Restaurant(Integer id, String name, Address address, Collection<Menu> menus) {
-        super(id, name);
-        this.address = address;
-        setMenus(menus);
-        address.setRestaurant(this);
-    }
-
-    public Restaurant(Integer id, String name, Address address, Menu... menus) {
-        this(id, name, address, List.of(menus));
-    }
-
-    public void setAddress(Address address) {
-        this.address = address;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public Date getRegistered() {
-        return registered;
-    }
-
-    public void setRegistered(Date date) {
-        this.registered = date;
-    }
-
-    public boolean isEnabled() {
-        return enabled;
-    }
-
-    public void setEnabled(boolean enabled) {
-        this.enabled = enabled;
-    }
-
-    private List<Menu> setMenus(Collection<Menu> menus) {
-        return new LinkedList<>(menus);
-    }
-
-    public List<Menu> getMenuList() {
-        return menuList;
-    }
-
-    @Override
-    public String toString() {
-        return "Restaurant{" +
-                "name='" + name + '\'' +
-                ", id=" + id +
-                ", address=" + address +
-                '}';
-    }
+  @Column(name = "enabled", nullable = false, columnDefinition = "boolean default true")
+  private boolean enabled = true;
 }
